4ecb52ec341634f9f4222153871c18b95e8de3fa
Pradeep Banavara <pradeepbs@gmail.com>
2025-02-25 16:57:19 -0800

    Added Google SEO header for indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="zk5gMxHa7suS8vf_vaYhvy9w6fhm_IX9zUtVX1N9eG0" />
+      </head>
       <body>
         <SessionWrapper>
           {children}
